48efbdda20011274390239cf5450c676955c4b8a
carmim <gabrielpensador721@gmail.com>
2024-10-29 20:42:07 -0300

    alteração feita no Apostador, permite aceitar sem saldo

--- Apoio/Models/Apostador.cs
+++ Apoio/Models/Apostador.cs
@@ -1,11 +1,13 @@
-﻿namespace Apoio.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Apoio.Models
 {
     public class Apostador
     {
         public int Id { get; set; }
         public string  Nome { get; set; }
         public string Email { get; set; }
-        public decimal Saldo { get; set; }
+        public decimal? Saldo { get; set; }
 
 
     }
